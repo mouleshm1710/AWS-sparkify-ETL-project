e7ce9ebc95549390f4e71e6fe366ed0d36b33ca7
Moulesh M <68770713+mouleshm1710@users.noreply.github.com>
2024-08-02 19:53:07 -0600

    Update delete_cluster_resources.py

--- delete_cluster_resources.py
+++ delete_cluster_resources.py
@@ -1,4 +1,4 @@
-# import required libraries
+# Import required libraries
 import configparser
 import boto3
 import pandas as pd
@@ -8,64 +8,68 @@ import json
 config = configparser.ConfigParser()
 config.read_file(open('dwh.cfg'))
 
-KEY = config.get('AWS','KEY')
-SECRET = config.get('AWS','SECRET')
-SESSION_TOKEN = config.get('AWS','SESSION_TOKEN')
+KEY = config.get('AWS', 'KEY')
+SECRET = config.get('AWS', 'SECRET')
+SESSION_TOKEN = config.get('AWS', 'SESSION_TOKEN')
 
-DWH_CLUSTER_TYPE       = config.get("DWH","DWH_CLUSTER_TYPE")
-DWH_NUM_NODES          = config.get("DWH","DWH_NUM_NODES")
-DWH_NODE_TYPE          = config.get("DWH","DWH_NODE_TYPE")
-DWH_IAM_ROLE_NAME      = config.get("DWH", "DWH_IAM_ROLE_NAME")
-DWH_CLUSTER_IDENTIFIER = config.get("DWH","DWH_CLUSTER_IDENTIFIER")
-DWH_DB                 = config.get("DWH","DWH_DB")
-DWH_DB_USER            = config.get("DWH","DWH_DB_USER")
-DWH_DB_PASSWORD        = config.get("DWH","DWH_DB_PASSWORD")
-DWH_PORT               = config.get("DWH","DWH_PORT")
-DWH_ENDPOINT           = config.get("DWH","DWH_ENDPOINT")
-DWH_ROLE_ARN           = config.get("DWH","DWH_ROLE_ARN")
-DWH_VPC_ID             = config.get("DWH","DWH_VPC_ID")
+DWH_CLUSTER_TYPE = config.get("DWH", "DWH_CLUSTER_TYPE")
+DWH_NUM_NODES = config.get("DWH", "DWH_NUM_NODES")
+DWH_NODE_TYPE = config.get("DWH", "DWH_NODE_TYPE")
+DWH_IAM_ROLE_NAME = config.get("DWH", "DWH_IAM_ROLE_NAME")
+DWH_CLUSTER_IDENTIFIER = config.get("DWH", "DWH_CLUSTER_IDENTIFIER")
+DWH_DB = config.get("DWH", "DWH_DB")
+DWH_DB_USER = config.get("DWH", "DWH_DB_USER")
+DWH_DB_PASSWORD = config.get("DWH", "DWH_DB_PASSWORD")
+DWH_PORT = config.get("DWH", "DWH_PORT")
+DWH_ENDPOINT = config.get("DWH", "DWH_ENDPOINT")
+DWH_ROLE_ARN = config.get("DWH", "DWH_ROLE_ARN")
+DWH_VPC_ID = config.get("DWH", "DWH_VPC_ID")
 
-LOG_DATA               = config.get("S3","LOG_DATA")
-LOG_JSONPATH               = config.get("S3","LOG_JSONPATH")
-SONG_DATA               = config.get("S3","SONG_DATA")
+LOG_DATA = config.get("S3", "LOG_DATA")
+LOG_JSONPATH = config.get("S3", "LOG_JSONPATH")
+SONG_DATA = config.get("S3", "SONG_DATA")
 
 # Connection to AWS services (IAM & REDSHIFT)
+iam = boto3.client(
+    'iam',
+    aws_access_key_id=KEY,
+    aws_secret_access_key=SECRET,
+    aws_session_token=SESSION_TOKEN,
+    region_name='us-east-1'
+)
 
-iam = boto3.client('iam',aws_access_key_id=KEY,
-                     aws_secret_access_key=SECRET,
-                     aws_session_token=SESSION_TOKEN,  
-                     region_name='us-east-1')
+redshift = boto3.client(
+    'redshift',
+    region_name="us-east-1",
+    aws_access_key_id=KEY,
+    aws_secret_access_key=SECRET,
+    aws_session_token=SESSION_TOKEN
+)
 
-redshift = boto3.client('redshift',
-                       region_name="us-east-1",
-                       aws_access_key_id= KEY,
-                       aws_secret_access_key= SECRET,
-                       aws_session_token=SESSION_TOKEN
-                       )
-
-print("Connection to the AWS services is successfull ")
+print("Connection to the AWS services is successful.")
 print()
 
-
 # Delete resources & roles
-
-print("Lets Delete Existing IAM role: ")
+print("Let's delete existing IAM role: ")
 try:
-    iam.detach_role_policy(RoleName=DWH_IAM_ROLE_NAME, PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")
+    iam.detach_role_policy(
+        RoleName=DWH_IAM_ROLE_NAME,
+        PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+    )
     iam.delete_role(RoleName=DWH_IAM_ROLE_NAME)
-    print("Deletion of Role is successful")
+    print("Deletion of role is successful.")
     print()
 except Exception as e:
-    print("Error in Deleting the role")
+    print("Error in deleting the role:")
     print(e)
     print()
 
-print("Lets Delete any existing cluster: ")
+print("Let's delete any existing cluster: ")
 try:
-    redshift.delete_cluster(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,  SkipFinalClusterSnapshot=True)
-    print("cluster deleted")
+    redshift.delete_cluster(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER, SkipFinalClusterSnapshot=True)
+    print("Cluster deletion initiated.")
     print()
 except Exception as e:
-    print("Error in deleting the cluster")
+    print("Error in deleting the cluster:")
     print(e)
     print()
